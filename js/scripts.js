e3878a34820973e001b4ab80b564c9ecceff8429
dmanp3 <dpurnell@engr-134-13.eastern.ewu.edu>
2014-10-20 13:15:03 -0700

    Rounded boxes
    Roud box

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// Foundation 5 Activate! 
+$(document).foundation();
+
+
+// On page load, do this
+$(document).ready(function(){
+  
+  // Your Scripts
+$("#one").corner("bottom");
+$("#two").corner("bite tl");
+$("#three").corner("left notch");
+
+
+});
